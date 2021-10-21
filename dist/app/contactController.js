aa6198761260b23ba0989c14eed209db0561d8f8
Jayaprasanth Gurunathan <nandhanguru@gmail.com>
2021-10-21 11:57:26 -0400

    Edit and Update Doc

--- dist/app/contactController.js
+++ dist/app/contactController.js
@@ -16,12 +16,12 @@ app.controller('contactController', function ($scope) {
                 querySnapshot.forEach(function (doc) {
                     console.log(doc.id, " => ", doc.data());
                     const data = doc.data();
-                    const fbdocid = doc.id;
-                    const mixData =  { fbdocid, ...data };
+                    const id = doc.id;
+                    const mixData = { id, ...data };
                     list.push(mixData);
                 });
                 $scope.contactlist = list;
-                
+
                 $scope.$apply();
                 console.log($scope.contactlist);
             });
@@ -29,28 +29,58 @@ app.controller('contactController', function ($scope) {
 
     $scope.onSubmit = function (contactform) {
         // console.log($scope.contactform);
-        if(contactform != undefined) {
-        afsDB.collection("Contacts").doc().set(contactform)
-            .then(() => {
-                console.log("Document successfully written!");
-                $scope.onLoadData();
-                $scope.onResetForm();
-            })
-            .catch((error) => {
-                console.error("Error writing document: ", error);
-            });
+        if (contactform != undefined) {
+            console.log(contactform.id);
+            if (contactform.id != undefined) {
+                afsDB.collection("Contacts").doc(contactform.id).update(contactform)
+                    .then(() => {
+                        console.log("Document successfully updated!");
+                    }).catch((error) => {
+                        console.error("Error Updating document: ", error);
+                    });
+            } else {
+                afsDB.collection("Contacts").doc().set(contactform)
+                    .then(() => {
+                        console.log("Document successfully written!");
+
+                    })
+                    .catch((error) => {
+                        console.error("Error writing document: ", error);
+                    });
+            }
+            $scope.onLoadData();
+            $scope.onResetForm();
         }
         else {
             alert("Please enter contact name");
         }
     };
 
-    $scope.onEdit = function () {
-        alert("Edit Info");
+    $scope.onEdit = function (e) {
+        var id = $(e.target).data('id');
+        var name = $(e.target).data('name');
+        var phone = $(e.target).data('phone');
+        console.log(id);
+        console.log(name);
+        console.log(phone);
+        $scope.contactform = { "name": name, "phone": phone, "id": id };
+        
+
+
     };
 
-    $scope.onDelete = function () {
-        alert("Delete Info");
+    $scope.onDelete = function (e) {
+        var id = $(e.target).data('id');
+        console.log(id);
+
+
+        afsDB.collection("Contacts").doc(id).delete().then(() => {
+            console.log("Document successfully deleted!");
+            $scope.onLoadData();
+        }).catch((error) => {
+            console.error("Error removing document: ", error);
+        });
+
     };
 
     $scope.onSearchKey = function ($event) {
@@ -74,6 +104,6 @@ app.controller('contactController', function ($scope) {
 
     }
     $scope.onResetForm = function () {
-        $scope.contactform = "";
+        $scope.contactform = {};
     }
 });
